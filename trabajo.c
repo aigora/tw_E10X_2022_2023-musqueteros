ac7d4e1c20d6b161ee62106fe404f9bd31c482fd
ainaragarc <130687565+ainaragarc@users.noreply.github.com>
2023-05-23 17:38:22 +0200

    funcion contraseña trabajo.c
    prototipo de la funcion para lo de las contraseñas. Pone contrasena con n todo el rato porque no lee las ñ (lo digo por si os pasa como a mi)

--- trabajo.c
+++ trabajo.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
 	
+	printf("(introduccion del trabajo, explicaciones y contraseñas)\n\n");
+	
+	int vueltaalmenu1;
+	int comprobacion;
+    	const char *contrasenadosejemplo = "acelgas"; //para demomento nos vale
 	int vueltaalmenu;
+	
 	do {//bucle para volver al menu principal
 	
 	vueltaalmenu1=0;//en caso de volver necesitar volver al menu, se cambiara la variable por 1 y volvera al bucle
 	
-	printf("(introduccion del trabajo, explicaciones y contraseñas)\n\n");
 	printf("si quiere pasar a (parte de Jaime), pulse 1\n");
 	printf("si quiere pasar a (parte de Aianra), pulse 2\n");
 	printf("si quiere pasar a (parte de Diego), pulse 3\n");
@@ -24,7 +30,14 @@ int main(){
       break;
       
     case '2':
-      printf("parte ainara\n");
+	comprobacion = verificarcontrasena(contrasenadosejemplo);
+      if (comprobacion==1) {
+        printf("Contraseña correcta. Acceso concedido.\n");
+    } else {
+        printf("Contraseña incorrecta. Acceso denegado.\n");
+        vueltaalmenu1=1;
+        break;
+    }
       break;
       
     case '3':
@@ -46,4 +59,20 @@ int main(){
 	
 	}while(vueltaalmenu1!=0);
 	return 0;
+}
+
+int verificarcontrasena(const char contrasena[8]) //intento funcion contraseña
+{
+	int i=0, j=0;
+	char respuesta[10];
+    for (i=0; i<=2; i++){
+    	printf("porfavor introduzca la contraseña\nte quedan %i  intentos\n", 3-i);
+    	scanf("%s", respuesta);
+    if (strcmp(contrasena, respuesta) == 0) {
+    	j=1;
+        break; // las contraseñas son iguales asique es valida
+    }
+	}
+		return j;
+	
 }
